fe152086a43f2fff54122ea50ff41c0aad32fd99
Simon Madsen <simon@codespace.dk>
2020-06-16 08:41:32 +0200

    dsa

--- middleware.js
+++ middleware.js
@@ -7,29 +7,16 @@ samlify.setSchemaValidator(validator);
 
 const oktaIdpEnc = samlify.IdentityProvider({
   metadata: fs.readFileSync(__dirname + '/metadata/nemid.xml'),
-  isAssertionEncrypted: true,
-  messageSigningOrder: 'encrypt-then-sign',
-  wantLogoutRequestSigned: true
-});
+})
 
 const spEnc = samlify.ServiceProvider({
   entityID: 'https://kombit.codespace.dk/sp/metadata',
-  authnRequestsSigned: false,
-  wantAssertionsSigned: true,
-  wantMessageSigned: true,
-  wantLogoutResponseSigned: true,
-  wantLogoutRequestSigned: true,
-  privateKey: fs.readFileSync(__dirname + '/key/sign/privkey.pem'),
-  privateKeyPass: 'Test1234',
-  encPrivateKey: fs.readFileSync(__dirname + '/key/sign/privkey.pem'),
-  encPrivateKeyPass: 'Test1234',
-  encryptCert: fs.readFileSync(__dirname + '/key/sign/cert.cer'),
-  signingCert: fs.readFileSync(__dirname + '/key/sign/cert.cer'),
+  signingCert: fs.readFileSync(__dirname + '/key/cert.pem'),
+  encryptCert: fs.readFileSync(__dirname + '/key/cert.pem'),
   singleLogoutService: [{
     Binding: binding.redirect,
     Location: 'https://kombit.codespace.dk/sp/single_logout/redirect',
   }],
-  isAssertionEncrypted: true,
   assertionConsumerService: [{
     Binding: binding.post,
     Location: 'https://kombit.codespace.dk/sp/acs',
